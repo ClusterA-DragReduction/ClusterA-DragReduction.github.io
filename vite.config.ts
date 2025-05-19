4858ffa9ee3cb0c09636d6efb2fbdd4cde656b84
ClusterA-DragReduction <dragreduction@outlook.com>
2025-05-20 07:39:56 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  base: '/ClusterA-DragReduction.github.io/',
-});
+  base: '/', // 使用根路径以确保正确访问静态资源
+  build: {
+    outDir: 'docs' // 将输出目录改为docs
+  }
+})
